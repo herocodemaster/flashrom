herocodemaster/flashrom

commit 484700cd14b3c7cc3755494774b485404b862278

SST39SF512 is tested
flashrom -V -w: http://paste.flashrom.org/view.php?id=395

Signed-off-by: Michael Karcher <flashrom@mkarcher.dialup.fu-berlin.de>
Acked-by: Carl-Daniel Hailfinger <c-d.hailfinger.devel.2006@gmx.net>

git-svn-id: svn://flashrom.org/flashrom/trunk@1275 2b7e53f0-3cfb-0310-b3e9-8179ed1497e1

flashchips.c
@@ -5567,7 +5567,7 @@ struct flashchip flashchips[] = {
 		.total_size	= 64,
 		.page_size	= 4096,
 		.feature_bits	= FEATURE_EITHER_RESET,
-		.tested		= TEST_UNTESTED,
+		.tested		= TEST_PREW,
 		.probe		= probe_jedec,
 		.probe_timing	= 1,			/* 150 ns */
 		.block_erasers	=

commit 05f4582e298e499c3ae96be056f3daef40010144

Fix broken compilation caused by a typo in r1275.
Signed-off-by: Idwer Vollering <vidwer@gmail.com>
Acked-by: Idwer Vollering <vidwer@gmail.com>

flashchips.c
@@ -5567,7 +5567,7 @@ struct flashchip flashchips[] = {
 		.total_size	= 64,
 		.page_size	= 4096,
 		.feature_bits	= FEATURE_EITHER_RESET,
-		.tested		= TEST_PREW,
+		.tested		= TEST_OK_PREW,
 		.probe		= probe_jedec,
 		.probe_timing	= 1,			/* 150 ns */
 		.block_erasers	=